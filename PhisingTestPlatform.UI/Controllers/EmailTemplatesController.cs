b0afa55fea673e1122722915d42d491315b0e538
Yigit Akture <220706301@st.maltepe.edu.tr>
2023-12-16 16:00:17 +0300

    Initial commit

---
+++ PhisingTestPlatform.UI/Controllers/EmailTemplatesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhisingTestPlatform.UI.Controllers
+{
+    public class EmailTemplatesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
