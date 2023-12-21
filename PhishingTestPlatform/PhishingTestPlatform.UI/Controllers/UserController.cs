916983f62e97d38da200d0744457f21845be8ee8
Yigit Akture <220706301@st.maltepe.edu.tr>
2023-12-21 23:10:01 +0300

    Add login pages

--- PhishingTestPlatform/PhishingTestPlatform.UI/Controllers/UserController.cs
+++ PhishingTestPlatform/PhishingTestPlatform.UI/Controllers/UserController.cs
@@ -15,6 +15,24 @@ namespace PhishingTestPlatform.UI.Controllers
             return View("CreditCardInformation");
         }
 
+        [HttpGet("Spotify")]
+        public IActionResult SpotifyLogin()
+        {
+            return View("SpotifyLogin");
+        }
+
+        [HttpGet("Netflix")]
+        public IActionResult NetflixLogin()
+        {
+            return View("NetflixLogin");
+        }
+
+        [HttpGet("Twitter")]
+        public IActionResult TwitterLogin()
+        {
+            return View("TwitterLogin");
+        }
+
         public IActionResult CreditCard()
         {
             return View();
