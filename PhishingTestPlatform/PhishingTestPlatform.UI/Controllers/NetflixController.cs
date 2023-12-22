0456e7f5b1fd4d53423afb88cdede872009c9f33
Yigit Akture <220706301@st.maltepe.edu.tr>
2023-12-22 10:46:44 +0300

    Update Login and credit card pages

---
+++ PhishingTestPlatform/PhishingTestPlatform.UI/Controllers/NetflixController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhishingTestPlatform.UI.Controllers
+{
+    public class NetflixController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreditCard()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return RedirectToAction("CreditCard");
+        }
+
+        [HttpPost]
+        public IActionResult CreditCardSubmit()
+        {
+            return RedirectToAction("Index", "Error");
+        }
+    }
+}
